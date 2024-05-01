b60e3438b60364f737735d7eda946cc99090ae12
Nathan <georgenathan010@gmail.com>
2024-05-01 12:30:25 +0300

    added defining folder

---
+++ Starter/Log.cpp
@@ -0,0 +1,7 @@
+
+#include "Logger.h"
+#include <iostream>
+
+void Log (const char* message) {
+    std::cout << message;
+}
---
+++ Starter/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Logger.h"
+
+
+int main () {
+
+    Log("hello world");
+    std::cin.get();
+}
