acc7b628d3e9bb98efb7135304bcb1a80d96a588
Nathami-n <georgenathan010@gmail.com>
2024-05-13 15:51:52 +0300

    Added a splicer function

---
+++ c/String/string.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string.h>
+#include <stdio.h>
+typedef char* string;
+
+//splice function to splice the string
+
+void splicer(string input, int start, int end) {
+    char splicedString[200];
+    strcpy(splicedString, input);
+
+    while(start <= end) {
+        printf("%c", splicedString[start]);
+
+        start++;
+    }
+
+}
