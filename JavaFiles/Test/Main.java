197faba39243be21d9988715b546442e2d33ddc1
Nathami-n <georgenathan010@gmail.com>
2024-05-15 23:39:18 +0300

    Mulitdimensional Array

--- JavaFiles/Test/Main.java
+++ JavaFiles/Test/Main.java
@@ -1,10 +1,17 @@
-import java.util.Date;
+import java.util.*;
 public class Main {
     
     public static void main(String[] args) {
         Date date = new Date();  
         String message = new String("Hello");
         int[] numbers = new int[3];
+
+        //new method 
+        int[] numbers = {2,4,5,6};
+       int length =  numbers.length;
+       int[][] = new int[2][3]; 
+       Arrays.sort(numbers)
+        Arrays.toString(numbers);
          System.out.println(date);
     }
 }
