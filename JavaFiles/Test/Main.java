f0003b28430105d917a33f8fcb66eb87fd0732ea
Nathami-n <georgenathan010@gmail.com>
2024-05-15 23:31:01 +0300

    Added array

--- JavaFiles/Test/Main.java
+++ JavaFiles/Test/Main.java
@@ -3,6 +3,8 @@ public class Main {
     
     public static void main(String[] args) {
         Date date = new Date();  
+        String message = new String("Hello");
+        int[] numbers = new int[3];
          System.out.println(date);
     }
 }
