bb7b061370320ab95cfd23da3bb73869e9baf904
Nathami-n <georgenathan010@gmail.com>
2024-07-09 16:27:46 +0300

    Add: string methods

--- python/main.py
+++ python/main.py
@@ -2,3 +2,15 @@ import datetime;
 
 
 print(datetime.date(2024, 2, 1))
+
+# string methods practice
+
+name = "Nathan"
+
+# get given number of string
+print(name[1:2])
+print(name[:2])
+print(name[0:-1])
+print(name[::])
+print(name[::2])
+
